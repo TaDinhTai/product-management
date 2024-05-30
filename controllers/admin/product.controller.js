3a9d3935a8aa33f4965a3bf0e6114a2d8f2e3f26
TaDinhTai <tadinhtai6868@gmail.com>
2024-05-30 22:32:28 +0700

    Tối ưu phần bộ lọc

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -1,28 +1,33 @@
 const Product = require("../../models/product.model");
 
+const filterStatusHelper = require("../../helpers/filterStatus");
+
 // [GET] /admin/products
 module.exports.index = async (req, res) => {
 
-  let filterStatus = [
-    {
-      name: "Tất cả",
-      status: "",
-      class: ""
-    },
-    {
-      name: "Hoạt động",
-      status: "active",
-      class: ""
-    },
-    {
-      name: "Dừng hoạt động",
-      status: "inactive",
-      class: ""
-    },
-  ];
+  // let filterStatus = [
+  //   {
+  //     name: "Tất cả",
+  //     status: "",
+  //     class: ""
+  //   },
+  //   {
+  //     name: "Hoạt động",
+  //     status: "active",
+  //     class: ""
+  //   },
+  //   {
+  //     name: "Dừng hoạt động",
+  //     status: "inactive",
+  //     class: ""
+  //   },
+  // ];
 
 
   // console.log(req.query.status);
+
+  const filterStatus = filterStatusHelper(req.query);
+  console.log(filterStatus);
 
   let find = {
     deleted: false, 
@@ -42,14 +47,14 @@ module.exports.index = async (req, res) => {
   }
 
 
-  if(req.query.status) {
-    // find.status = req.query.status;
-    const index = filterStatus.findIndex(item => item.status == req.query.status);
-    filterStatus[index].class = "active";
-  } else {
-    const index = filterStatus.findIndex(item => item.status == "");
-    filterStatus[index].class = "active";
-  }
+  // if(req.query.status) {
+  //   // find.status = req.query.status;
+  //   const index = filterStatus.findIndex(item => item.status == req.query.status);
+  //   filterStatus[index].class = "active";
+  // } else {
+  //   const index = filterStatus.findIndex(item => item.status == "");
+  //   filterStatus[index].class = "active";
+  // }
 
   const products = await Product.find(find);
 
