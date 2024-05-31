133899a9e601e840233f16d7bebd29922e3ef81d
TaDinhTai <tadinhtai6868@gmail.com>
2024-05-31 22:25:09 +0700

    Tính năng phân trang

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -2,12 +2,15 @@ const Product = require("../../models/product.model");
 
 const filterStatusHelper = require("../../helpers/filterStatus");
 const searchHelper = require("../../helpers/search");
+const paginationHelper = require("../../helpers/pagination");
+
 
 // [GET] /admin/products
 module.exports.index = async (req, res) => {
 
+  //Filter
   const filterStatus = filterStatusHelper(req.query);
-  console.log(filterStatus);
+  // console.log(filterStatus);
 
   let find = {
     deleted: false, 
@@ -17,18 +20,48 @@ module.exports.index = async (req, res) => {
     find.status = req.query.status;
   }
 
+  //Search
   const objectSearch = searchHelper(req.query);
 
   if(objectSearch.regex) {
     find.title = objectSearch.regex;
   }
 
-  const products = await Product.find(find);
+  //Pagination
+  const countProducts = await Product.countDocuments(find);
+
+  let objectPagination = paginationHelper(
+    {
+    currentPage: 1,
+    limitItems: 4
+    },
+    req.query,
+    countProducts
+);
+
+  // let objectPagination = {
+  //   currentPage: 1,
+  //   limitItems: 4
+  // };
+
+  // if(req.query.page) {
+  //   objectPagination.currentPage = parseInt(req.query.page);
+  // }
+
+  // objectPagination.skip = (objectPagination.currentPage - 1) * objectPagination.limitItems;
+
+  // const countProducts = await Product.countDocuments(find);
+  // const totalPage = Math.ceil(countProducts/objectPagination.limitItems);
+  // objectPagination.totalPage = totalPage;
+  //End Pagination
+
+  const products = await Product.find(find).limit(objectPagination.limitItems).skip(objectPagination.skip);
 
   res.render("admin/pages/products/index", {
     pageTitle: "Danh sách sản phẩm",
     products: products,
     filterStatus: filterStatus,
     keyword: objectSearch.keyword,
+    pagination: objectPagination
   });
 };
