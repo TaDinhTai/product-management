006592878e1485711d1695e5421caf5c94c00a9f
TaDinhTai <tadinhtai6868@gmail.com>
2024-05-31 09:59:11 +0700

    Tối ưu phần tìm kiếm

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -1,30 +1,10 @@
 const Product = require("../../models/product.model");
 
 const filterStatusHelper = require("../../helpers/filterStatus");
+const searchHelper = require("../../helpers/search");
 
 // [GET] /admin/products
 module.exports.index = async (req, res) => {
-
-  // let filterStatus = [
-  //   {
-  //     name: "Tất cả",
-  //     status: "",
-  //     class: ""
-  //   },
-  //   {
-  //     name: "Hoạt động",
-  //     status: "active",
-  //     class: ""
-  //   },
-  //   {
-  //     name: "Dừng hoạt động",
-  //     status: "inactive",
-  //     class: ""
-  //   },
-  // ];
-
-
-  // console.log(req.query.status);
 
   const filterStatus = filterStatusHelper(req.query);
   console.log(filterStatus);
@@ -37,33 +17,18 @@ module.exports.index = async (req, res) => {
     find.status = req.query.status;
   }
 
-  let keyword = "";
+  const objectSearch = searchHelper(req.query);
 
-  if(req.query.keyword) {
-    keyword = req.query.keyword;
-
-    const regex = new RegExp(keyword, "i");
-    find.title = regex;
+  if(objectSearch.regex) {
+    find.title = objectSearch.regex;
   }
 
-
-  // if(req.query.status) {
-  //   // find.status = req.query.status;
-  //   const index = filterStatus.findIndex(item => item.status == req.query.status);
-  //   filterStatus[index].class = "active";
-  // } else {
-  //   const index = filterStatus.findIndex(item => item.status == "");
-  //   filterStatus[index].class = "active";
-  // }
-
   const products = await Product.find(find);
-
-  //console.log(products);
 
   res.render("admin/pages/products/index", {
     pageTitle: "Danh sách sản phẩm",
     products: products,
     filterStatus: filterStatus,
-    keyword: keyword,
+    keyword: objectSearch.keyword,
   });
 };
---
+++ helpers/search.js
@@ -0,0 +1,16 @@
+module.exports = (query) => {
+  // let keyword = "";
+  let objectSearch = {
+    keyword: "",
+  }
+
+  if(query.keyword) {
+    objectSearch.keyword = query.keyword;
+
+    const regex = new RegExp(objectSearch.keyword, "i");
+    objectSearch.regex = regex;
+    // find.title = regex;
+  }
+
+  return objectSearch;
+}
