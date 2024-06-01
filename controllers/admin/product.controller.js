40703cfb45b2caf64a785b54e581773d20c6c1bb
TaDinhTai <tadinhtai6868@gmail.com>
2024-06-01 09:36:54 +0700

    Thay đổi trạng thái nhiều sản phẩm

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -66,7 +66,7 @@ module.exports.index = async (req, res) => {
   });
 };
 
-// [GET] /admin/products/change-staus/:status/:id
+// [PATCH] /admin/products/change-staus/:status/:id
 module.exports.changeStatus = async (req, res) => {
   const status = req.params.status;
   const id = req.params.id;
@@ -76,4 +76,24 @@ module.exports.changeStatus = async (req, res) => {
   // res.send(`${status} - ${id}`);
   // res.redirect("/admin/products");
   res.redirect('back');
+}
+
+// [PATCH] /admin/products/change-multi
+module.exports.changeMulti = async (req, res) => {
+  const type = req.body.type;
+  const ids = req.body.ids.split(", ");
+
+  switch (type) {
+    case "active":
+      await Product.updateMany({_id: {$in : ids}}, {status: "active"})
+      break;
+    case "inactive":
+      await Product.updateMany({_id: {$in : ids}}, {status: "inactive"})
+      break;
+  
+    default:
+      break;
+  }
+
+  res.redirect("back");
 }
